5fb119bd5d24b55adab514f975d22a4410e79685
David Molineus <david.molineus@netzmacht.de>
2015-01-26 13:48:01 +0100

    Fix paths and namespaces.

--- module/config/config.php
+++ module/config/config.php
@@ -9,9 +9,9 @@
 
 
 // Content elements
-$GLOBALS['TL_CTE']['bootstrap_tabs']['bootstrap_tabStart'] = 'Netzmacht\Bootstrap\Components\Contao\ContentElement\Tab';
-$GLOBALS['TL_CTE']['bootstrap_tabs']['bootstrap_tabPart']  = 'Netzmacht\Bootstrap\Components\Contao\ContentElement\Tab';
-$GLOBALS['TL_CTE']['bootstrap_tabs']['bootstrap_tabEnd']   = 'Netzmacht\Bootstrap\Components\Contao\ContentElement\Tab';
+$GLOBALS['TL_CTE']['bootstrap_tabs']['bootstrap_tabStart'] = 'Netzmacht\Bootstrap\Tab\TabElement';
+$GLOBALS['TL_CTE']['bootstrap_tabs']['bootstrap_tabPart']  = 'Netzmacht\Bootstrap\Tab\TabElement';
+$GLOBALS['TL_CTE']['bootstrap_tabs']['bootstrap_tabEnd']   = 'Netzmacht\Bootstrap\Tab\TabElement';
 
 // Wrapper settings
 $GLOBALS['TL_WRAPPERS']['start'][]     = 'bootstrap_tabStart';
